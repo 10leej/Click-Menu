ecbc1e77fdb11c216d6f3a39e8845a8295d6ce24
Joshua Lee <10leej@gmail.com>
2019-10-31 05:12:45 -0400

    git catchup
    classic conversion

--- core.lua
+++ core.lua
@@ -1,19 +1,13 @@
-local ClickMenu, namespace = ...
-local L = namespace.L
-
---Our one and only config option, should probably add this in game
-local MouseButton = 'RightButton' --LeftButton, RightButton, MiddleButton
-
---End config
-local menuFrame = CreateFrame('Frame', 'ClickMenu', UIParent, 'UIDropDownMenuTemplate')
+-- Right click menu
+local menuFrame = CreateFrame("Frame", "BobMinimapRightClickMenu", UIParent, "UIDropDownMenuTemplate")
 local menuList = {
     {
-		text = L['Micro Menu'],
-		isTitle = true,
-		notCheckable = true,
-	},
+        text = "MicroMenu",
+        isTitle = true,
+        notCheckable = true,
+    },
     {
-        text = L['Character'],
+        text = "Character",
         icon = 'Interface\\PaperDollInfoFrame\\UI-EquipmentManager-Toggle',
         func = function()
             securecall(ToggleCharacter, 'PaperDollFrame')
@@ -21,15 +15,17 @@ local menuList = {
         notCheckable = true,
     },
     {
-        text = L['Spellbook'],
+        text = "Spellbook",
         icon = 'Interface\\MINIMAP\\TRACKING\\Class',
         func = function()
             securecall(ToggleSpellBook, BOOKTYPE_SPELL)
         end,
+        tooltipTitle = securecall(MicroButtonTooltipText, SPELLBOOK_ABILITIES_BUTTON, 'TOGGLESPELLBOOK'),
+        tooltipText = NEWBIE_TOOLTIP_SPELLBOOK,
         notCheckable = true,
     },
     {
-        text = L['Specialization'],
+        text = "Talents",
         icon = 'Interface\\MINIMAP\\TRACKING\\Ammunition',
         func = function()
             if (not PlayerTalentFrame) then
@@ -42,36 +38,22 @@ local menuList = {
 
             securecall(ToggleTalentFrame)
         end,
+        tooltipTitle = securecall(MicroButtonTooltipText, TALENTS_BUTTON, 'TOGGLETALENTS'),
+        tooltipText = NEWBIE_TOOLTIP_TALENTS,
         notCheckable = true,
     },
     {
-        text = L['Inventory'],
-        icon = 'Interface\\MINIMAP\\TRACKING\\Banker',
+        text = "Quest Log",
+        icon = 'Interface\\GossipFrame\\ActiveQuestIcon',
         func = function()
-            securecall(ToggleAllBags)
+            securecall(ToggleFrame, QuestLogFrame)
         end,
+        tooltipTitle = securecall(MicroButtonTooltipText, QUESTLOG_BUTTON, 'TOGGLEQUESTLOG'),
+        tooltipText = NEWBIE_TOOLTIP_QUESTLOG,
         notCheckable = true,
     },
     {
-        text = L['Achievements'],
-        icon = 'Interface\\ACHIEVEMENTFRAME\\UI-Achievement-Shield',
-        func = function()
-            securecall(ToggleAchievementFrame)
-        end,
-        notCheckable = true,
-    },
-	{
-		text = L['Map & Quest Log'],
-		icon = 'Interface\\GossipFrame\\ActiveQuestIcon',
-		func = function()
-			securecall(ToggleFrame, WorldMapFrame)
-		end,
-			tooltipTitle = securecall(MicroButtonTooltipText, QUESTLOG_BUTTON, 'TOGGLEQUESTLOG'),
-			tooltipText = NEWBIE_TOOLTIP_QUESTLOG,
-			notCheckable = true,
-	},
-    {
-        text = L['Social'],
+        text = "Friends",
         icon = 'Interface\\FriendsFrame\\PlusManz-BattleNet',
         func = function()
             securecall(ToggleFriendsFrame, 1)
@@ -79,94 +61,70 @@ local menuList = {
         notCheckable = true,
     },
     {
-        text = L['Guild'],
+        text = "Guild",
         icon = 'Interface\\GossipFrame\\TabardGossipIcon',
         arg1 = IsInGuild('player'),
         func = function()
             if (IsTrialAccount()) then
                 UIErrorsFrame:AddMessage(ERR_RESTRICTED_ACCOUNT, 1, 0, 0)
             else
-                securecall(ToggleGuildFrame)
+                securecall(ToggleFriendsFrame, 3)
             end
         end,
         notCheckable = true,
     },
     {
-        text = L['Group Finder'],
-        icon = 'Interface\\LFGFRAME\\BattleNetWorking0',
-        func = function()
-            securecall(PVEFrame_ToggleFrame, 'GroupFinderFrame', LFDParentFrame)
-        end,
-        notCheckable = true,
-    },
-	{
-		text = L['Player vs. Player'], --broke
-		icon = 'Interface\\MINIMAP\\TRACKING\\BattleMaster',
-		func = function()
-			securecall(PVEFrame_ToggleFrame, 'PVPUIFrame', HonorFrame)
-		end,
-		tooltipTitle = securecall(MicroButtonTooltipText, PLAYER_V_PLAYER, 'TOGGLECHARACTER4'),
-		tooltipText = NEWBIE_TOOLTIP_PVP,
-		notCheckable = true,
-	},
-    {
-        text = L['Dungeon Journal'],
-        icon = 'Interface\\MINIMAP\\TRACKING\\Profession',
-        func = function()
-            securecall(ToggleEncounterJournal)
-        end,
-        notCheckable = true,
-    },
-    {
-        text = L['Collections'],
-        icon = 'Interface\\MINIMAP\\TRACKING\\StableMaster',
-        func = function()
-            securecall(TogglePetJournal, 1)
-        end,
-        notCheckable = true,
-    },
-	{
-		text = L["Calender"],--broke
-		icon = 'Interface\\Calendar\\UI-Calendar-Button',
-		func = function()
-			LoadAddOn('Blizzard_Calendar')
-			Calendar_Toggle()
-		end,
-		notCheckable = true,
-	},
-    {
-        text = L['Shop'],
-        icon = 'Interface\\MINIMAP\\TRACKING\\BattleMaster',
-        func = function()
-            if (not StoreFrame) then
-                LoadAddOn('Blizzard_StoreUI')
-            end
-            securecall(ToggleStoreUI)
-        end,
-        notCheckable = true,
-    },
-    {
-        text = L['Support'],
+        text = "Support",
         icon = 'Interface\\CHATFRAME\\UI-ChatIcon-Blizz',
         func = function()
             securecall(ToggleHelpFrame)
         end,
+        tooltipTitle = HELP_BUTTON,
+        tooltipText = NEWBIE_TOOLTIP_HELP,
         notCheckable = true,
     },
---[[ Need to fix, was broke in 8.0
+	   {
+        text = "Game Menu",
+        isTitle = true,
+        notCheckable = true,
+    },
     {
-        text = L['Battlefield Map'],
+        text = "System",
         func = function()
-            securecall(ToggleBattlefieldMinimap)
+            VideoOptionsFrame:Show()
         end,
         notCheckable = true,
     },
-]]
-
+    {
+        text = "Interface",
+        func = function()
+            InterfaceOptionsFrame:Show()
+        end,
+        notCheckable = true,
+    },
+    {
+        text = "Macro",
+        func = function()
+            ShowMacroFrame()
+        end,
+        notCheckable = true,
+    },
+    {
+        text = "Key Bindings",
+        func = function()
+			if (not KeyBindingFrame) then
+				KeyBindingFrame_LoadUI()
+			end
+            KeyBindingFrame:Show()
+        end,
+        notCheckable = true,
+    },
 }
-Minimap:SetScript('OnMouseUp', function(self, button)
-	if button == MouseButton then
-		securecall(EasyMenu, menuList, menuFrame, self, 27, 190, 'MENU', 8)	
+
+--Make it work
+Minimap:SetScript("OnMouseUp", function(self, btn)
+	if btn == "RightButton" then
+		EasyMenu(menuList, menuFrame, "cursor", 0, 0, "MENU", 2)	
 	else
 		Minimap_OnClick(self)
 	end
